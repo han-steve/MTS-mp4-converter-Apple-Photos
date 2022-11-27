21fc9c2d16ad615b1af49f422ee08b356e1af613
Steve Han <stevehan2001@gmail.com>
2022-11-26 18:54:28 -0600

    skeleton for os.exec test

--- pkg/converter/converter_test.go
+++ pkg/converter/converter_test.go
@@ -1,7 +1,26 @@
 package converter_test
 
-import "testing"
+import (
+	"os"
+	"os/exec"
+	"testing"
+)
 
 func TestRightCommand(t *testing.T) {
 
+}
+
+func fakeExecCommand(command string, args...string) *exec.Cmd {
+	cs := []string{"-test.run=TestHelperProcess", "--", command}
+	cs = append(cs, args...)
+	cmd := exec.Command(os.Args[0], cs...)
+	cmd.Env = []string{"GO_WANT_HELPER_PROCESS=1"}
+	return cmd
+}
+
+func TestHelperProcess(t *testing.T) {
+	if os.Getenv("GO_WANT_HELPER_PROCESS") != "1" {
+		return
+	}
+	os.Exit(0)
 }
